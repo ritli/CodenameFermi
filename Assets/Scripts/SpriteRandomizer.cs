8b06974e0e4c9543e836a54602c161337a0384c7
Nerfertius <nerfertius@gmail.com>
2018-08-09 20:37:07 +0200

    Updated scenes with new level design, finished level draft of planet

---
+++ Assets/Scripts/SpriteRandomizer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRandomizer : MonoBehaviour {
+
+    public Sprite[] sprites;
+    bool spriteRandomized = false;
+
+    Vector3 lastpos = Vector3.zero;
+
+    /*
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (lastpos != transform.position)
+        {
+            lastpos = transform.position;
+
+            GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+        }
+
+        lastpos = transform.position;
+    }
+#endif
+    */
+    void Start () {
+           GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+        Destroy(this);
+    }
+
+}
