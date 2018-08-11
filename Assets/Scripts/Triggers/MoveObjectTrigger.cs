09b17c34d59bc97c29b7ecfbdb61c2fc592a9863
ritli <agnar_oskarsson@hotmail.com>
2018-08-11 14:26:41 +0200

    Work on in-scene triggers

---
+++ Assets/Scripts/Triggers/MoveObjectTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class MoveObjectTrigger : MonoBehaviour, ITrigger
+{
+    public float time;
+
+    public Transform objectToMove;
+    public Transform destination;
+
+#if UNITY_EDITOR
+
+    void OnValidate()
+    {
+        if (!transform.Find("Destintation") && !destination)
+        {
+            print(transform.Find("Destination"));
+            destination = new GameObject("Destination").transform;
+            destination.transform.parent = transform;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (objectToMove && destination)
+        {
+            Debug.DrawLine(objectToMove.position, destination.position);
+        }
+    }
+
+#endif
+
+    public void OnEventFinished()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StartTrigger()
+    {
+        objectToMove.DOMove(destination.position, time);
+    }
+}
